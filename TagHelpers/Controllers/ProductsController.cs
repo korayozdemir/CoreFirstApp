1bcd59e024475dbca74f1fa3e648acd1a05a647a
korayozdemir <koray_ozdemir@msn.com>
2020-05-04 12:50:28 +0300

    Add project files.

---
+++ TagHelpers/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TagHelpers.Models;
+
+namespace TagHelpers.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(ProductRepository.Products);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            ProductRepository.AddProduct(product);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TagHelpers/Models/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagHelpers.Models
+{
+    public static class ProductRepository
+    {
+        private static List<Product> _products = new List<Product>();
+
+        public static List<Product> Products
+        {
+            get { return _products; }
+        }
+
+        public static void AddProduct(Product product)
+        {
+            _products.Add(product);
+        }
+
+    }
+}
