669980ec4a6498a5eda46e50f21ce518f4139360
korayozdemir <koray_ozdemir@msn.com>
2020-05-06 12:59:28 +0300

    test

--- CoreViewComponent/Controllers/ProductsController.cs
+++ CoreViewComponent/Controllers/ProductsController.cs
@@ -1,26 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using CoreViewComponent.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreViewComponent.Controllers
 {
-    public class HomeController : Controller
+    public class ProductsController : Controller
     {
         private readonly IProductRepository repo;
-        public HomeController(IProductRepository _repo)
+        public ProductsController(IProductRepository _repo)
         {
             repo = _repo;
         }
-
         public IActionResult Index()
         {
-            return View(repo.Products);
+            return View();
         }
-
     }
 }
--- CoreViewComponent/Startup.cs
+++ CoreViewComponent/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreViewComponent.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -22,7 +23,9 @@ namespace CoreViewComponent
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddSingleton<IProductRepository, ProductRepository>();
+            services.AddSingleton<ICategoryRepository, CategoryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
